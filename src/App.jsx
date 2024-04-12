6553cd533d7c76cb19dcc758dd3a3662ddfa5585
architp28 <160946784+architp28@users.noreply.github.com>
2024-04-13 01:09:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import InputBox from './components/InputBox'
-// import useCurrencyInfo from './hooks/useCurrencyInfo'
+import useCurrencyInfo from './hooks/useCurrencyInfo' 
 import './App.css';
 
 function App() {
+    const [amount , setamount] = useState(1);
+    const [from , setfrom] = useState("inr");
+    const [to , setto]  = useState("usd");
+    const currenies = useCurrencyInfo(from);
+    const options = Object.keys(currenies);
+    // console.log(currenies);
     return (
         <div className="w-full">
                 <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
@@ -14,8 +20,11 @@ function App() {
                     >
                         <div className="w-full mb-1">
                             <InputBox
-                                label="From"
-                                
+                                label="From"    
+                                amount = {amount}
+                                onamountchange={(amount) => setamount(amount)}
+                                selectcurrency={from}
+                                allcurrency={options}
                             />
                         </div>
                         <div className="relative w-full h-0.5">
@@ -29,6 +38,7 @@ function App() {
                         </div>
                         <div className="w-full mt-1 mb-4">
                             <InputBox
+
                                 label="To"
                                 
                             />
